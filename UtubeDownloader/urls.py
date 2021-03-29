9ed95305c5f597cd090193b94323818f700beb02
Rakesh Gombi <rakeshgombi.ec19@rvce.edu.in>
2021-03-30 01:45:44 +0530

    Update urls.py

--- UtubeDownloader/urls.py
+++ UtubeDownloader/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.views import static
+from django.views.static import serve
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="Home"),
-    path('search/', views.search, name="Search")]
+    path('search/', views.search, name="Search"),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    ]
